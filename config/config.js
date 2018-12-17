a2dc33ced526f46f858b981fcfd5180313831699
afanti <shuo.zhao@lejent.com>
2018-12-17 15:49:58 +0800

    update

--- config/config.js
+++ config/config.js
@@ -2,11 +2,39 @@ export default {
     singular: true,
     routes: [{
         path: '/',
-        component: './HelloWorld',
+        component: '../layout',
+        routes: [
+            {
+                path: '/',
+                component: './HelloWorld'
+            },
+            {
+                path: '/helloworld',
+                component: './HelloWorld'
+            },
+            {
+                path: '/dashboard',
+                routes: [
+                    {
+                        path: '/dashboard/analysis',
+                        component: './Dashboard/Analysis'
+                    },
+                    {
+                        path: '/dashboard/monitor',
+                        component: './Dashboard/Monitor'
+                    },
+                    {
+                        path: '/dashboard/workplace',
+                        component: './Dashboard/Workplace'
+                    }
+                ]
+            }
+        ]
     }],
-    plugins:[
-        ['umi-plugin-react',{
-            antd:true
+    plugins: [
+        ['umi-plugin-react', {
+            antd: true,
+            dav: true
         }]
     ]
 };
